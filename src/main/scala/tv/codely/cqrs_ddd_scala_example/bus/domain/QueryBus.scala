3c6968ece2fcdd54840b9265361da71f1e3ea41f
Rafa Gómez <rgomezcasas@gmail.com>
2017-09-13 23:21:57 +0200

    Remove sync and async query bus and generate a typed class one

--- src/main/scala/tv/codely/cqrs_ddd_scala_example/bus/domain/QueryBus.scala
+++ src/main/scala/tv/codely/cqrs_ddd_scala_example/bus/domain/QueryBus.scala
@@ -1,5 +1,9 @@
 package tv.codely.cqrs_ddd_scala_example.bus.domain
 
-trait QueryBus[P[_], QueryType <: Query, ResponseType <: Response] {
-  def ask(query: QueryType): P[ResponseType]
+import scala.reflect.ClassTag
+
+final class QueryBus[P[_], QueryType <: Query, ResponseType <: Response](
+  private val handlers: Map[ClassTag[QueryType], QueryHandler[P, QueryType, ResponseType]]
+) {
+  def ask(query: QueryType): P[ResponseType] = handlers(ClassTag[QueryType](query.getClass)).handle(query)
 }
