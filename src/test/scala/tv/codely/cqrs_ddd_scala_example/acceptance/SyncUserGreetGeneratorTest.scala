8dd3c3f079006f0df73cfa3106a08ead48909863
Rafa Gómez <rgomezcasas@gmail.com>
2017-09-13 23:21:57 +0200

    Remove sync and async query bus and generate a typed class one

--- src/test/scala/tv/codely/cqrs_ddd_scala_example/acceptance/SyncUserGreetGeneratorTest.scala
+++ src/test/scala/tv/codely/cqrs_ddd_scala_example/acceptance/SyncUserGreetGeneratorTest.scala
@@ -13,7 +13,7 @@ import tv.codely.cqrs_ddd_scala_example.user_greet.infrastructure.InMemoryUserRe
 
 final class SyncUserGreetGeneratorTest extends WordSpec with GivenWhenThen {
 
-  "UserGreetGenerator with an SyncQueryBus" should {
+  "UserGreetGenerator with an SyncQueryBuas" should {
     "block the execution flow until getting a response from the repository" in {
 
       Given("a UserGreetGenerator with a user repository")
